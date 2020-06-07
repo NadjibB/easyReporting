37a7b8bd5de2065b65fb0b334b419d971d1908b2
NadjibB <nadjiiib@hotmail.fr>
2020-06-07 13:51:15 +0200

    ajout fonction upload fichiers

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinydashboard)
 library(shinydashboardPlus)
+library(tidyverse)
 
 ui <- dashboardPage(skin = 'black',
     dashboardHeader(title = "KPI RH"),
@@ -8,13 +9,28 @@ ui <- dashboardPage(skin = 'black',
     dashboardBody(
         
         
-        fileInput(inputId = "fichier_brut" , label = 'Fichier Brut', multiple = FALSE, accept = NULL, width = NULL)
-        
-        
+        fileInput(inputId = "fichier_brut" , label = 'Fichier Brut', multiple = TRUE, buttonLabel = "Upload...",
+                  accept = ".csv"),
+        tableOutput("donnees")
         
     )
 )
 
-server <- function(input, output) { }
+server <- function(input, output) { 
+    
+    output$donnees <- renderTable(input$fichier_brut)
+    
+    data <- reactive({
+        req(input$fichier_brut)
+        read_csv2(input$fichier_brut$datapath)
+        
+        })
+    
+    output$donnees <- renderTable(head(data(), 3))
+    
+    
+    
+    
+    }
 
 shinyApp(ui, server)
